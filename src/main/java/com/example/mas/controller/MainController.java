e3f14335a1bd035473cc46046f9b90465bf91179
ella.shin <ella.shin@seerstech.com>
2022-02-10 17:09:44 +0900

    main

--- src/main/java/com/example/mas/controller/MainController.java
+++ src/main/java/com/example/mas/controller/MainController.java
@@ -6,16 +6,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value = "/mas")
 public class MainController {
-    @RequestMapping(name="/mas/main")
+    @RequestMapping(value="/main")
     public String mainboard(Model model){
         Resource resource = new Resource();
         initailResource(resource);
         model.addAttribute("resource", resource);
-        return  "thymeleaf/mas/mainMas";
+        return  "thymeleaf/mas/mainMas.html";
     }
 
-    /** 자원 초기화 */
     private void initailResource(Resource resource){
         resource.setGeneration(0);
         resource.setO2(0);
